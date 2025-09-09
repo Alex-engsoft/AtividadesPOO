f87e426078edccde386913af283ae6623eb17d81
Alex <engs-alexsandrolemos@camporeal.edu.br>
2025-09-09 20:08:59 -0300

    Add files via upload

---
+++ ativ aula 09/geometri.java
@@ -0,0 +1,41 @@
+interface FormaGeometrica {
+    double calcularArea();
+}
+
+class Circulo implements FormaGeometrica {
+    private double raio;
+
+    public Circulo(double raio) {
+        this.raio = raio;
+    }
+
+    @Override
+    public double calcularArea() {
+        return Math.PI * raio * raio;
+    }
+}
+
+class Retangulo implements FormaGeometrica {
+    private double largura;
+    private double altura;
+
+    public Retangulo(double largura, double altura) {
+        this.largura = largura;
+        this.altura = altura;
+    }
+
+    @Override
+    public double calcularArea() {
+        return largura * altura;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        FormaGeometrica circulo = new Circulo(5);
+        FormaGeometrica retangulo = new Retangulo(4, 6);
+
+        System.out.println("Área do círculo: " + circulo.calcularArea());
+        System.out.println("Área do retângulo: " + retangulo.calcularArea());
+    }
+}
